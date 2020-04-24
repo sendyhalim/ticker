b1b560dee0a628269e4606281aa3968c593fa053
Sendy Halim <sendyhalim93@gmail.com>
2020-04-25 02:21:17 +0700

    feat(experimental): Enable multiple tickers on different lines

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,43 @@
 use futures_util::future;
 use futures_util::pin_mut;
 use futures_util::StreamExt;
+use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 
-use serde_json::Value as JsonValue;
-use tokio::io::AsyncReadExt;
+use crossterm::cursor;
+use crossterm::execute;
+use crossterm::ExecutableCommand;
+use serde::{Deserialize, Serialize};
 use tokio::io::AsyncWriteExt;
 use tokio::prelude::*;
-use tokio::sync::Mutex;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::error::Error as WebSocketError;
 use tokio_tungstenite::tungstenite::protocol::Message as WebSocketMessage;
 use url::Url;
 
-struct Counter {
-  val: i32,
+#[derive(Debug)]
+struct CursorPosition {
+  row: u16,
+  col: u16,
+  relative: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TickerPayload {
+  s: String,
+  c: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct StreamPayload {
+  stream: String,
+  data: TickerPayload,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let url = "wss://stream.binance.com:9443/ws/btcusdt@ticker";
+  let url = "wss://stream.binance.com:9443/stream?streams=btcusdt@ticker/adausdt@ticker";
 
   println!("Connecting to {} ...", url);
 
@@ -34,16 +51,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   let (_, ws_stream_reader) = ws_stream.split();
 
+  let mut cursor_position_by_stream: HashMap<&str, CursorPosition> = HashMap::new();
+
+  let (_, y) = cursor::position()?;
+
+  cursor_position_by_stream.insert(
+    "btcusdt@ticker",
+    CursorPosition {
+      row: y - 1,
+      col: 0,
+      relative: 2,
+    },
+  );
+  cursor_position_by_stream.insert(
+    "adausdt@ticker",
+    CursorPosition {
+      row: y - 2,
+      col: 0,
+      relative: 1,
+    },
+  );
+
+  println!("----------------------");
+  println!();
+  println!();
+
   ws_stream_reader
     .for_each(|message: Result<WebSocketMessage, WebSocketError>| {
       async {
         match message {
           Ok(message) => {
-            let body: JsonValue = serde_json::from_str(message.to_text().unwrap()).unwrap();
+            let body: StreamPayload = serde_json::from_str(message.to_text().unwrap()).unwrap();
+            let cursor_position = cursor_position_by_stream.get(&body.stream[..]).unwrap();
+            let stdout = io::stdout();
 
-            print!("Ticker: {}\r", body["c"]);
-
+            execute!(io::stdout(), cursor::MoveUp(cursor_position.relative));
+            print!("\r{}: {}\r", body.data.s, body.data.c);
             io::stdout().flush().unwrap();
+            execute!(io::stdout(), cursor::MoveDown(cursor_position.relative));
           }
           Err(err) => println!("Error: {}", err),
         }
