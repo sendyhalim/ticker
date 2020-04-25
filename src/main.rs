8e3d12161d8fe7521a19ca24f23d757af59a3a03
Sendy Halim <sendyhalim93@gmail.com>
2020-04-25 12:49:48 +0700

    feat(dynamic ticker): Use loop to make ticker dynamic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use futures_util::future;
-use futures_util::pin_mut;
 use futures_util::StreamExt;
 use std::collections::HashMap;
 use std::io;
@@ -7,10 +5,8 @@ use std::io::Write;
 
 use crossterm::cursor;
 use crossterm::execute;
-use crossterm::ExecutableCommand;
 use serde::{Deserialize, Serialize};
-use tokio::io::AsyncWriteExt;
-use tokio::prelude::*;
+use serde_json::Result as SerdeResult;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::error::Error as WebSocketError;
 use tokio_tungstenite::tungstenite::protocol::Message as WebSocketMessage;
@@ -18,8 +14,7 @@ use url::Url;
 
 #[derive(Debug)]
 struct CursorPosition {
-  row: u16,
-  col: u16,
+  ticker_symbol: String,
   relative: u16,
 }
 
@@ -37,11 +32,44 @@ struct StreamPayload {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let url = "wss://stream.binance.com:9443/stream?streams=btcusdt@ticker/adausdt@ticker";
+  let tickers: Vec<String> = vec!["btcusdt", "adausdt", "iotausdt", "xlmusdt"]
+    .iter()
+    .map(|symbol_pair| {
+      return format!("{}@ticker", symbol_pair);
+    })
+    .collect();
+
+  let stream_params: String = tickers.join("/");
+
+  let url = format!(
+    "wss://stream.binance.com:9443/stream?streams={}",
+    stream_params
+  );
+
+  let ticker_range = std::ops::Range {
+    start: 0,
+    end: tickers.len(),
+  };
+
+  let ticker_cursors: Vec<(String, CursorPosition)> = ticker_range
+    .map(|index| {
+      let ticker_symbol = tickers.get(index).unwrap();
+
+      let cursor_position = CursorPosition {
+        ticker_symbol: String::from(ticker_symbol),
+        relative: (tickers.len() - index) as u16,
+      };
+
+      return (String::from(ticker_symbol), cursor_position);
+    })
+    .collect();
+
+  let cursor_position_by_stream: HashMap<String, CursorPosition> =
+    ticker_cursors.into_iter().collect();
 
   println!("Connecting to {} ...", url);
 
-  let url = Url::parse(url).unwrap();
+  let url = Url::parse(&url).unwrap();
 
   println!("Opening websocket...");
 
@@ -51,44 +79,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   let (_, ws_stream_reader) = ws_stream.split();
 
-  let mut cursor_position_by_stream: HashMap<&str, CursorPosition> = HashMap::new();
-
-  let (_, y) = cursor::position()?;
-
-  cursor_position_by_stream.insert(
-    "btcusdt@ticker",
-    CursorPosition {
-      row: y - 1,
-      col: 0,
-      relative: 2,
-    },
-  );
-  cursor_position_by_stream.insert(
-    "adausdt@ticker",
-    CursorPosition {
-      row: y - 2,
-      col: 0,
-      relative: 1,
-    },
-  );
+  println!("{:?}", cursor_position_by_stream);
 
   println!("----------------------");
-  println!();
-  println!();
+  print_lines(&tickers);
 
   ws_stream_reader
     .for_each(|message: Result<WebSocketMessage, WebSocketError>| {
       async {
         match message {
           Ok(message) => {
-            let body: StreamPayload = serde_json::from_str(message.to_text().unwrap()).unwrap();
-            let cursor_position = cursor_position_by_stream.get(&body.stream[..]).unwrap();
-            let stdout = io::stdout();
+            let response_text = message.to_text().unwrap();
+            let body: SerdeResult<StreamPayload> = serde_json::from_str(response_text);
 
-            execute!(io::stdout(), cursor::MoveUp(cursor_position.relative));
+            if body.is_err() {
+              println!("Error when deserializing {}", response_text);
+              print_lines(&tickers);
+              return;
+            }
+
+            let body = body.unwrap();
+
+            let cursor_position = cursor_position_by_stream.get(&body.stream[..]).unwrap();
+
+            execute!(io::stdout(), cursor::MoveUp(cursor_position.relative)).unwrap();
+
             print!("\r{}: {}\r", body.data.s, body.data.c);
             io::stdout().flush().unwrap();
-            execute!(io::stdout(), cursor::MoveDown(cursor_position.relative));
+
+            execute!(io::stdout(), cursor::MoveDown(cursor_position.relative)).unwrap();
           }
           Err(err) => println!("Error: {}", err),
         }
@@ -97,4 +116,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await;
 
   return Ok(());
+}
+
+fn print_lines(tickers: &Vec<String>) {
+  println!(
+    "{}",
+    tickers.iter().map(|_| "\n").collect::<Vec<&str>>().join("")
+  );
 }
